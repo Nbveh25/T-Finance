108dde8382f629171a159f1fd914e2f0a339b500
Timur Bikmukhametov <tima.bikmukhametov@inbox.ru>
2025-04-22 14:31:13 +0300

    unused imports deleted

--- app/src/main/java/ru/practice/t_finance/presentation/screens/AuthScreen.kt
+++ app/src/main/java/ru/practice/t_finance/presentation/screens/AuthScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -19,7 +18,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
